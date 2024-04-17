5e8b2d450e4848d9eb064357072695bbfd69692d
tekkac <tarik@carbonable.io>
2024-04-17 19:47:07 +0200

    ✨ add anon wallet + layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useAccount, useConnect } from "@starknet-react/core";
+import { useAccount, useConnect, Connector } from "@starknet-react/core";
 import { useCallback, useState } from "react";
 import Image from 'next/image'
 import logo from '../../public/logo.svg'
@@ -8,13 +8,14 @@ import Wallet from "../components/wallet";
 
 
 export default function Home() {
-  const { account, address, status } = useAccount();
+  const { status } = useAccount();
   const [isReady, setIsReady] = useState(false);
+  const [isAnon, setIsAnon] = useState<boolean>(false);
   const onClick = useCallback(() => {
     setIsReady(true);
   }, []);
 
-  if (status === "connected") {
+  if (status === "connected" || isAnon) {
     return (
       <main className="flex min-h-screen flex-col items-center p-20 pt-10 relative">
         <Wallet />
@@ -30,14 +31,18 @@ export default function Home() {
         <Image src={logo} alt="Amneszia" />
       </div>
       <div className="flex place-items-center mt-24">
-        {!isReady && <button onClick={onClick} className="button">Ready ?</button>}
-        {isReady && <WalletList />}
+        {!isReady && <button onClick={onClick} className="button">Ready?</button>}
+        {isReady && <WalletList setIsAnon={setIsAnon} />}
       </div>
     </main>
   );
 }
 
-function WalletList() {
+interface WalletListProps {
+  setIsAnon(data: boolean): void;
+}
+
+function WalletList(props: WalletListProps) {
   const { connect, connectors } = useConnect();
   return (
     <ul>
@@ -48,6 +53,11 @@ function WalletList() {
           </button>
         </li>
       ))}
-    </ul>
+      <li key="anon" className="pr-12 inline-block">
+        <button className="button" onClick={() => props.setIsAnon(true)}>
+          anon
+        </button>
+      </li>
+    </ul >
   );
 }
